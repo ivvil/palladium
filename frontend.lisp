422b0117c09a61d6c78ddcde463b68e71451bcc0
Iván Villagrasa <ivvil412@gmail.com>
2024-06-26 23:36:58 +0200

    start adding list view and fixed creation of events

--- frontend.lisp
+++ frontend.lisp
@@ -2,15 +2,29 @@
 
 (in-package #:palladium)
 
+(defparameter *events-per-page* 25)
 
 (define-page dbadd "palladium/dbadd" (:clip "dbadd.ctml")
   (r-clip:process T))
-
 
 (define-page dbview "palladium/dbview/(.*)" (:uri-groups (id) :clip "dbview.ctml")
   (r-clip:process
    T
    :action (ensure-event id)))
+
+(define-page raw "palladium/view/(.*)/raw" (:uri-groups (id))
+  (setf (content-type *response*) "text/plain")
+  (dm:field (ensure-event id) "data"))
+
+(define-page list "plaster/list(?:/(.*))?" (:uri-groups (page) :clip "list.ctml")
+  (let* ((page (or (when page (parse-integer page :junk-allowed T)) 0))
+         (events (dm:get 'palladium-actions (db:query :all)
+                         :sort '((time :DESC))
+                         :skip (* page *events-per-page*)
+                         :amount *events-per-page*)))
+    (r-clip:process T :events events
+                      :page page
+                      :has-more (<= *events-per-page* (length events)))))
 
 (defun ensure-event (evt-ish)
   (typecase evt-ish
@@ -21,9 +35,10 @@
 
 (defun create-event (event data)
   (let ((action (dm:hull 'palladium-actions)))
-	(setf (dm:field action "event") event
-		  (dm:field action "time") (get-universal-time)
-		  (dm:field action "data") data)))
+    (setf (dm:field action "event") event
+          (dm:field action "data") data
+          (dm:field action "time") (get-universal-time))
+    (dm:insert action)))
 
 (define-api palladium/new (event data) ()
   (let ((evt (create-event event data)))
